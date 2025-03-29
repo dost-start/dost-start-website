980a07179f84896c35eec6c3993f728b6675ab2f
John Roice Aldeza <jroicealdeza@gmail.com>
2025-03-29 21:41:13 +0800

    fix(PageTitle): center align text for improved visual presentation

--- src/components/PageTitle.tsx
+++ src/components/PageTitle.tsx
@@ -1,6 +1,6 @@
 export default function PageTitle({ text }: { text?: string }) {
   return (
-    <h1 className="text-muted-foreground font-orbitron text-2xl md:text-3xl my-6">
+    <h1 className="text-muted-foreground font-orbitron text-2xl md:text-3xl my-6 text-center">
       {text}
     </h1>
   );
