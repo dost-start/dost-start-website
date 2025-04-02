b10b22b2d023e9b0f32bc8e2c4dcab592635a8c7
JohnPatrickAncajas <143093214+JohnPatrickAncajas@users.noreply.github.com>
2025-04-02 20:40:49 +0800

    adapted both version of officers page

--- src/lib/officers/2024-2025/crrd.ts
+++ src/lib/officers/2024-2025/crrd.ts
@@ -1,11 +1,10 @@
-
 import { Department } from "@/types/officerType";
 
 const department: Department = {
   name: "Community & Regional Relations Department",
   tabName: "CRRD",
   description:
-    "The Communications Department is responsible for managing the organization's social media accounts, website, and email communications.",
+    "The Community & Regional Relations Department focuses on fostering positive relationships with local communities and regional partners, ensuring engagement and support for organizational initiatives.",
   specialOfficers: [],
   officers: [],
 };
--- src/lib/officers/2024-2025/executive.ts
+++ src/lib/officers/2024-2025/executive.ts
@@ -1,11 +1,10 @@
-
 import { Department } from "@/types/officerType";
 
 const department: Department = {
   name: "Executive Leadership",
   tabName: "Executive",
   description:
-    "The Communications Department is responsible for managing the organization's social media accounts, website, and email communications.",
+    "The Executive Leadership department is responsible for overseeing the strategic direction of the organization, guiding key decisions, and ensuring the alignment of all departments with the organizations goals.",
   specialOfficers: [],
   officers: [],
 };
