6dbb34111e9dac5b69281099ab92872b36dd9bb0
KingEddeh <28296038+KingEddeh@users.noreply.github.com>
2025-05-10 00:18:15 +0800

    add communcations officer data

--- src/lib/officers/2024-2025/communications.ts
+++ src/lib/officers/2024-2025/communications.ts
@@ -1,5 +1,50 @@
 import { Department } from "@/types/officerType";
-import json from "./json/communications.json";
 
-const department: Department = json;
+const department: Department = {
+  name: "Communications Department",
+  tabName: "Communication",
+  description: "The Communications Department is responsible for managing the organization's social media accounts, website, and email communications.",
+  specialOfficers: [
+    {
+      name: "Anasel Caneso",
+      position: "Deputy Chief Communications Officer",
+      imageSrc: "https://i.imgur.com/Z1ZxJfZ.png",
+      socialLinks: {
+        facebook: "fb.com/eynasilaceey",
+        linkedin: "http://linkedin.com/in/anasel-ace",
+        instagram: "https://www.instagram.com/anaselace",
+      }
+    }
+  ],
+  officers: [
+    {
+      name: "Marvin James A. Erosa",
+      position: "Partnership Lead",
+      imageSrc: "https://i.imgur.com/m7pajph.jpeg",
+      socialLinks: {
+        facebook: "https://www.facebook.com/marvinjameserosa",
+        github: "https://github.com/marvinjameseros",
+        linkedin: "https://www.linkedin.com/in/marvinjameserosa/",
+      }
+    },
+    {
+      name: "Anthony Karl M. Miranda",
+      position: "External Management Officer",
+      imageSrc: "https://media1.tenor.com/m/_M07E1kX-DMAAAAC/hammy-brian-hairliners.gif",
+      socialLinks: {
+        facebook: "https://www.facebook.com/akmrndd",
+        linkedin: "https://www.linkedin.com/in/akmrnd/",
+        instagram: "https://www.instagram.com/akmrnd_/",
+      }
+    },
+    {
+      name: "Andrei Rouiz Pascual",
+      position: "Chief Communications Officer",
+      imageSrc: "https://media1.tenor.com/m/_M07E1kX-DMAAAAC/hammy-brian-hairliners.gif",
+      socialLinks: {
+        twitter: "https://www.facebook.com/pscl.ndr"
+      }
+    },
+  ]
+};
 export default department;
