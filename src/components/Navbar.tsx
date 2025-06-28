cf289976a7aef3ac78de374e1afd7a5c18553fc6
John Roice Aldeza <jroicealdeza@gmail.com>
2025-06-28 18:49:12 +0800

    feat: wrap logo image in a link for improved navigation

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,12 +1,15 @@
 import Image from "next/image";
 import MenuItems from "./MenuItems";
 import logo from "../../public/logo.png";
+import Link from "next/link";
 
 export default function Navbar() {
   return (
     <div className="sticky top-0 shadow-md bg-background z-40">
       <div className="max-w-[1720px] px-4 py-4 flex items-center justify-between   mx-auto">
-        <Image src={logo} alt="Start Logo" height={50} />
+        <Link href={"/"}>
+          <Image src={logo} alt="Start Logo" height={50} />
+        </Link>
         <MenuItems />
       </div>
     </div>
