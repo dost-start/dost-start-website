a230b924d5f8e6a724dc874a495d532b2d456017
John Roice Aldeza <jroicealdeza@gmail.com>
2025-07-02 11:55:26 +0800

    feat: update past events with new onboarding session details and add event display image

--- src/lib/events/past.ts
+++ src/lib/events/past.ts
@@ -2,7 +2,95 @@ import Event from "@/types/eventType";
 
 const pastEvents: Event[] = [
   {
-    title: "KickSTART Pre-Event Series: “Scholars Bridging Vision and Innovation for Technological Transformation.”",
+    title: "START Onboarding Session 2025",
+    slug: "start-onboarding-session-2025",
+    tags: [],
+    description:
+      "The START Onboarding Session 2025 is a virtual orientation program designed to welcome, equip, and empower the first batch of techno-scholars with the organization’s visions, missions, core initiatives, and internal structures while fostering early engagement through interactive and meaningful connections and community platform.",
+    location: "",
+    date: new Date("2025-04-09"),
+    startingTime: "10:00 AM",
+    endingTime: "12:00 PM",
+    registrationLink: "",
+    socialLinks: {
+      facebook:
+        "https://m.facebook.com/story.php?story_fbid=pfbid0GFH3KzbDEG9EvDMws2rc3qEpFAVVyF2kF3eJFwTMAAucioY5PgxF6uRBezZG7rq2l&id=61568353648492&mibextid=ZbWKwL",
+      instagram: "",
+      website: "",
+    },
+    hashtags: [
+      "START",
+      "DOSTSEI",
+      "TechForAllRegions",
+      "InnovationThroughCollaboration",
+      "ISKOLARNGBAYAN",
+      "ScholarsForInnovation",
+    ],
+    coverImage:
+      "https://res.cloudinary.com/dsz9ok0yq/image/upload/v1747205655/onboarding_cover_plxg7y.jpg",
+    eventDisplayImage:
+      "https://res.cloudinary.com/dsz9ok0yq/image/upload/v1747205660/Onboardinfg__25_01_pmzazy.jpg",
+    images: [
+      "https://res.cloudinary.com/dsz9ok0yq/image/upload/v1747205660/Onboardinfg__25_01_pmzazy.jpg",
+      "https://res.cloudinary.com/dsz9ok0yq/image/upload/v1747205661/Onboardinfg__25_02_n274ry.jpg",
+      "https://res.cloudinary.com/dsz9ok0yq/image/upload/v1747205662/Onboardinfg__25_03_mv1tow.jpg",
+      "https://res.cloudinary.com/dsz9ok0yq/image/upload/v1747205662/Onboardinfg__25_04_ebw9gh.jpg",
+      "https://res.cloudinary.com/dsz9ok0yq/image/upload/v1747205663/Onboardinfg__25_05_fx353z.jpg",
+    ],
+  },
+  {
+    title:
+      "KickSTART Luzon: “Scholars Bridging Vision and Innovation for Technological Transformation.”",
+    slug: "kickstart-luzon-2025",
+    tags: [],
+    description:
+      "KickSTART Luzon was a groundbreaking tech gathering, bringing together 100 techno-scholars from the island. This face-to-face general assembly served as a platform for collaboration, knowledge exchange, and alignment with national technological initiatives.\n\nAs the first event of its kind, KickSTART Luzon introduced scholars to Scholars Transforming Advancement and Research for Technology (START), emphasizing its mission to drive innovation and tech empowerment. This assembly served as a pivotal moment for scholars to connect, exchange ideas, and contribute to the future of technology-driven advancements for the nation’s progress.",
+    location:
+      "Arthaland Century Pacific Tower, 5th Avenue corner 30th Street, Bonifacio Global City, Taguig 1634, Philippines",
+    date: new Date("2025-02-10"),
+    startingTime: "09:00 AM",
+    endingTime: "06:00 PM",
+    registrationLink: "",
+    socialLinks: {
+      facebook: "https://www.facebook.com/share/p/18hnqjtPGA/",
+      instagram: "",
+      website: "",
+    },
+    hashtags: [
+      "KickSTART2025",
+      "TechScholarsUnite",
+      "START",
+      "DOSTSEI",
+      "TechForAllRegions",
+      "InnovationThroughCollaboration",
+      "ISKOLARNGBAYAN",
+      "ScholarsForInnovation",
+    ],
+    coverImage:
+      "https://res.cloudinary.com/dsz9ok0yq/image/upload/v1747205116/kickstart_luzon_cover_jasbkn.jpg",
+    eventDisplayImage:
+      "https://res.cloudinary.com/dsz9ok0yq/image/upload/v1751428347/kickstart-luzon_wnbkmn.jpg",
+    images: [
+      "https://res.cloudinary.com/dsz9ok0yq/image/upload/v1747205119/KickSTART_Luzon__25_02_fapuer.jpg",
+      "https://res.cloudinary.com/dsz9ok0yq/image/upload/v1747205120/KickSTART_Luzon__25_03_ylnnnr.jpg",
+      "https://res.cloudinary.com/dsz9ok0yq/image/upload/v1747205120/KickSTART_Luzon__25_05A_oodysg.jpg",
+      "https://res.cloudinary.com/dsz9ok0yq/image/upload/v1747205121/KickSTART_Luzon__25_05B_eekp6r.jpg",
+      "https://res.cloudinary.com/dsz9ok0yq/image/upload/v1747205122/KickSTART_Luzon__25_04_bkcfws.jpg",
+      "https://res.cloudinary.com/dsz9ok0yq/image/upload/v1747205123/KickSTART_Luzon__25_06A_mqmy36.jpg",
+      "https://res.cloudinary.com/dsz9ok0yq/image/upload/v1747205123/KickSTART_Luzon__25_06B_n9kl8d.jpg",
+      "https://res.cloudinary.com/dsz9ok0yq/image/upload/v1747205123/KickSTART_Luzon__25_07_zvyqoy.jpg",
+      "https://res.cloudinary.com/dsz9ok0yq/image/upload/v1747205123/KickSTART_Luzon__25_01_muemsb.jpg",
+      "https://res.cloudinary.com/dsz9ok0yq/image/upload/v1747205124/KickSTART_Luzon__25_08_lckuu6.jpg",
+      "https://res.cloudinary.com/dsz9ok0yq/image/upload/v1747205124/KickSTART_Luzon__25_09A_oidcxc.jpg",
+      "https://res.cloudinary.com/dsz9ok0yq/image/upload/v1747205125/KickSTART_Luzon__25_09B_kmgwsg.jpg",
+      "https://res.cloudinary.com/dsz9ok0yq/image/upload/v1747205125/KickSTART_Luzon__25_11_cymb85.jpg",
+      "https://res.cloudinary.com/dsz9ok0yq/image/upload/v1747205126/KickSTART_Luzon__25_10_yu8nok.jpg",
+      "https://res.cloudinary.com/dsz9ok0yq/image/upload/v1747205126/KickSTART_Luzon__25_12_x8pc8p.jpg",
+    ],
+  },
+  {
+    title:
+      "KickSTART Pre-Event Series: “Scholars Bridging Vision and Innovation for Technological Transformation.”",
     slug: "kickstart-pre-event-series-2025",
     tags: [],
     description:
@@ -26,9 +114,12 @@ const pastEvents: Event[] = [
       "TechForAllRegions",
       "InnovationThroughCollaboration",
       "ISKOLARNGBAYAN",
-      "ScholarsForInnovation"
+      "ScholarsForInnovation",
     ],
-    coverImage: "https://res.cloudinary.com/dsz9ok0yq/image/upload/v1747204191/kickstart_cover_vwnlbo.jpg",
+    coverImage:
+      "https://res.cloudinary.com/dsz9ok0yq/image/upload/v1747204191/kickstart_cover_vwnlbo.jpg",
+    eventDisplayImage:
+      "https://res.cloudinary.com/dsz9ok0yq/image/upload/v1747204200/KickSTART_Pre_Event_01_mapgab.jpg",
     images: [
       "https://res.cloudinary.com/dsz9ok0yq/image/upload/v1747204200/KickSTART_Pre_Event_02_qzdalt.jpg",
       "https://res.cloudinary.com/dsz9ok0yq/image/upload/v1747204200/KickSTART_Pre_Event_01_mapgab.jpg",
@@ -44,84 +135,6 @@ const pastEvents: Event[] = [
       "https://res.cloudinary.com/dsz9ok0yq/image/upload/v1747204205/KickSTART_Pre_Event_012_yz13vx.jpg",
     ],
   },
-  {
-    title: "KickSTART Luzon: “Scholars Bridging Vision and Innovation for Technological Transformation.”",
-    slug: "kickstart-luzon-2025",
-    tags: [],
-    description:
-      "KickSTART Luzon was a groundbreaking tech gathering, bringing together 100 techno-scholars from the island. This face-to-face general assembly served as a platform for collaboration, knowledge exchange, and alignment with national technological initiatives.\n\nAs the first event of its kind, KickSTART Luzon introduced scholars to Scholars Transforming Advancement and Research for Technology (START), emphasizing its mission to drive innovation and tech empowerment. This assembly served as a pivotal moment for scholars to connect, exchange ideas, and contribute to the future of technology-driven advancements for the nation’s progress.",
-    location: "Arthaland Century Pacific Tower, 5th Avenue corner 30th Street, Bonifacio Global City, Taguig 1634, Philippines",
-    date: new Date("2025-02-10"),
-    startingTime: "09:00 AM",
-    endingTime: "06:00 PM",
-    registrationLink: "",
-    socialLinks: {
-      facebook: "https://www.facebook.com/share/p/18hnqjtPGA/",
-      instagram: "",
-      website: "",
-    },
-    hashtags: [
-      "KickSTART2025",
-      "TechScholarsUnite",
-      "START",
-      "DOSTSEI",
-      "TechForAllRegions",
-      "InnovationThroughCollaboration",
-      "ISKOLARNGBAYAN",
-      "ScholarsForInnovation"
-    ],
-    coverImage: "https://res.cloudinary.com/dsz9ok0yq/image/upload/v1747205116/kickstart_luzon_cover_jasbkn.jpg",
-    images: [
-      "https://res.cloudinary.com/dsz9ok0yq/image/upload/v1747205119/KickSTART_Luzon__25_02_fapuer.jpg",
-      "https://res.cloudinary.com/dsz9ok0yq/image/upload/v1747205120/KickSTART_Luzon__25_03_ylnnnr.jpg",
-      "https://res.cloudinary.com/dsz9ok0yq/image/upload/v1747205120/KickSTART_Luzon__25_05A_oodysg.jpg",
-      "https://res.cloudinary.com/dsz9ok0yq/image/upload/v1747205121/KickSTART_Luzon__25_05B_eekp6r.jpg",
-      "https://res.cloudinary.com/dsz9ok0yq/image/upload/v1747205122/KickSTART_Luzon__25_04_bkcfws.jpg",
-      "https://res.cloudinary.com/dsz9ok0yq/image/upload/v1747205123/KickSTART_Luzon__25_06A_mqmy36.jpg",
-      "https://res.cloudinary.com/dsz9ok0yq/image/upload/v1747205123/KickSTART_Luzon__25_06B_n9kl8d.jpg",
-      "https://res.cloudinary.com/dsz9ok0yq/image/upload/v1747205123/KickSTART_Luzon__25_07_zvyqoy.jpg",
-      "https://res.cloudinary.com/dsz9ok0yq/image/upload/v1747205123/KickSTART_Luzon__25_01_muemsb.jpg",
-      "https://res.cloudinary.com/dsz9ok0yq/image/upload/v1747205124/KickSTART_Luzon__25_08_lckuu6.jpg",
-      "https://res.cloudinary.com/dsz9ok0yq/image/upload/v1747205124/KickSTART_Luzon__25_09A_oidcxc.jpg",
-      "https://res.cloudinary.com/dsz9ok0yq/image/upload/v1747205125/KickSTART_Luzon__25_09B_kmgwsg.jpg",
-      "https://res.cloudinary.com/dsz9ok0yq/image/upload/v1747205125/KickSTART_Luzon__25_11_cymb85.jpg",
-      "https://res.cloudinary.com/dsz9ok0yq/image/upload/v1747205126/KickSTART_Luzon__25_10_yu8nok.jpg",
-      "https://res.cloudinary.com/dsz9ok0yq/image/upload/v1747205126/KickSTART_Luzon__25_12_x8pc8p.jpg",
-    ],
-  },
-  {
-    title: "START Onboarding Session 2025",
-    slug: "start-onboarding-session-2025",
-    tags: [],
-    description:
-      "The START Onboarding Session 2025 is a virtual orientation program designed to welcome, equip, and empower the first batch of techno-scholars with the organization’s visions, missions, core initiatives, and internal structures while fostering early engagement through interactive and meaningful connections and community platform.",
-    location: "",
-    date: new Date("2025-04-09"),
-    startingTime: "10:00 AM",
-    endingTime: "12:00 PM",
-    registrationLink: "",
-    socialLinks: {
-      facebook: "https://m.facebook.com/story.php?story_fbid=pfbid0GFH3KzbDEG9EvDMws2rc3qEpFAVVyF2kF3eJFwTMAAucioY5PgxF6uRBezZG7rq2l&id=61568353648492&mibextid=ZbWKwL",
-      instagram: "",
-      website: "",
-    },
-    hashtags: [
-      "START",
-      "DOSTSEI",
-      "TechForAllRegions",
-      "InnovationThroughCollaboration",
-      "ISKOLARNGBAYAN",
-      "ScholarsForInnovation"
-    ],
-    coverImage: "https://res.cloudinary.com/dsz9ok0yq/image/upload/v1747205655/onboarding_cover_plxg7y.jpg",
-    images: [
-      "https://res.cloudinary.com/dsz9ok0yq/image/upload/v1747205660/Onboardinfg__25_01_pmzazy.jpg",
-      "https://res.cloudinary.com/dsz9ok0yq/image/upload/v1747205661/Onboardinfg__25_02_n274ry.jpg",
-      "https://res.cloudinary.com/dsz9ok0yq/image/upload/v1747205662/Onboardinfg__25_03_mv1tow.jpg",
-      "https://res.cloudinary.com/dsz9ok0yq/image/upload/v1747205662/Onboardinfg__25_04_ebw9gh.jpg",
-      "https://res.cloudinary.com/dsz9ok0yq/image/upload/v1747205663/Onboardinfg__25_05_fx353z.jpg",
-    ],
-  }  
 ];
 
 export default pastEvents;
